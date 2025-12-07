26630874eb6d11a2aff17858dbbfc50cc59b3dcc
Murugesh <murugeshvj@gmail.com>
2025-12-07 12:04:37 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,21 @@
+let message: string = "hello world";
+console.log(message);
+
+let age: number = 25;
+
+let isAdmin: boolean = true;
+
+let arrValues: string[] = ["sam", "kumar"];
+
+let studntInfo: {
+  name: string;
+  age: number;
+} = {
+  name: "sam",
+  age: 10.1
+};
+
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
+console.log(addNumbers(1, 12));
